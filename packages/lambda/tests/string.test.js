b34af9cf8eecacae177224bdc0091658e4a35ca2
--global <ragnarecek@gmail.com>
2019-05-06 10:24:54 +1200

    finished string test coverage

--- packages/lambda/tests/string.test.js
+++ packages/lambda/tests/string.test.js
@@ -25,4 +25,12 @@ test('testRegEx ouputs true if string b passes regular expression a.', () => {
 test('substr outputs substring based on provided string, start and limit.', () => {
   expect(λ.substr(3)(1)('7urtle')).toBe('urt');
   expect(λ.substr(1)(0)('7urtle')).toBe('7');
+  expect(λ.substr(1)(-1)('7urtle')).toBe('e');
+  expect(λ.substr(1)(1)('7urtle')).not.toBe('7');
+});
+
+test('startsWith outputs true if an input string starts with provided string.', () => {
+  expect(λ.startsWith('7')('7urtle')).toBe(true);
+  expect(λ.startsWith('7urtl')('7urtle')).toBe(true);
+  expect(λ.startsWith('8urtl')('7urtle')).toBe(false);
 });
