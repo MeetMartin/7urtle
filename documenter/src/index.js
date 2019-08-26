990efa56041a4833a527c201ea5a5c9df924f8b5
martin.novak <ragnarecek@gmail.com>
2019-08-26 23:39:47 +1200

    documenter continues

--- documenter/src/index.js
+++ documenter/src/index.js
@@ -1,4 +1,16 @@
-import {SyncEffect, Either, either, Maybe, maybe, identity} from "@7urtle/lambda";
+import {
+  SyncEffect,
+  Either,
+  either,
+  Maybe,
+  Case,
+  trim,
+  identity,
+  compose,
+  startsWith,
+  lengthOf,
+  substr, isEqual
+} from "@7urtle/lambda";
 import createLogger from "@7urtle/logger";
 import commander from 'commander';
 import fs from 'fs';
@@ -17,36 +29,80 @@ const program =
 
 const commanderFailed = logger => error => logger.error(`Documenter failed during initialisation with error: ${error}.`);
 
-const getInput = program =>
+const getFSStatus = path => Either.try(() => fs.statSync(path));
+const isFile = fsStatus => fsStatus.isLeft() ? false : fsStatus.value.isFile();
+
+const states = {
+  ERROR: 'ERROR',
+  CODE: 'CODE',
+  DESCRIPTION: 'DESCRIPTION',
+  TAG: 'TAG',
+  EXAMPLE: 'EXAMPLE'
+};
+
+let documentation = {
+  state: states.CODE,
+  contents: {
+    description: [],
+    tags: {},
+    example: []
+  }
+};
+
+const getDocumentationLineContents = line =>
+  (length => length > 1 ? trim(substr(length)(1)(line)) : '')
+  (lengthOf(line));
+
+const processLineCodeState = line =>
+  isEqual('/**')(line)
+    ? documentation.state = states.DESCRIPTION
+    : false;
+
+const processLineDescriptionState = line =>
+  startsWith('*')(line)
+    ? isEqual('*/')(line)
+        ? documentation.state = states.CODE
+        : (contents => lengthOf(contents) > 1 ? documentation.contents.description.push(contents) : false)
+          (getDocumentationLineContents(line))
+    : documentation.state = states.ERROR;
+
+const processLineBasedOnState = state =>
+  Case
+  .of([
+    [states.ERROR, identity],
+    [states.CODE, processLineCodeState],
+    [states.DESCRIPTION, processLineDescriptionState]
+  ])
+  .match(state);
+
+const processLine = line => processLineBasedOnState(documentation.state)(trim(line));
+
+const onFileEnd = logger => documentation => () => logger.debug(JSON.stringify(documentation));
+
+const lineReader = path =>
+  readline
+  .createInterface({input: fs.createReadStream(path)})
+  .on('line', processLine)
+  .on('close', onFileEnd(logger)(documentation));
+
+const lineReaderWithExceptionProcessing = logger => path => {
+  logger.info(`Processing file "${path}".`);
+  return either
+  (error => logger.error(`There was an error processing file "${path}" with exception: "${error}".`))
+  (identity)
+  (Either.try(() => lineReader(path)));
+};
+
+const getInput = logger => program =>
   (input =>
     input.isNothing()
       ? logger.error('Input is required argument. Try: $ documenter --input ./your/directory')
-      : logger.info(`Correct input ${input.value}`)
+      : compose(isFile, getFSStatus)(input.value)
+        ? lineReaderWithExceptionProcessing(logger)(input.value)
+        : logger.error('Only files are supported.')
   )(Maybe.of(program.input));
 
 either
 (commanderFailed(logger))
-(getInput)
+(getInput(logger))
 (Either.try(program.trigger));
-
-fs.statSync('../lambda/src/core.js').isFile();
-
-const rl = readline.createInterface({
-  input: fs.createReadStream('../lambda/src/core.js')
-});
-
-let line_no = 0;
-
-// event is emitted after each line
-rl.on('line', function(line) {
-  line_no++;
-  console.log(line);
-});
-
-// end
-rl.on('close', function(line) {
-  console.log('Total lines : ' + line_no);
-});
-//console.log(path.basename(path.dirname(fs.realpathSync(__filename))));
-//logger.log(fs.realpathSync(__filename));
-//logger.log(fs.realpathSync('../lambda/src/core.js'));
