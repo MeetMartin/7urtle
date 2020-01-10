fce58e5bea71ebda6f925d322f88cf022dc0fc35
Martin Novak <ragnarecek@gmail.com>
2020-01-10 13:52:53 +1300

    processing of tags and examples

--- documenter/src/index.js
+++ documenter/src/index.js
@@ -12,7 +12,8 @@ import {
   search,
   substr,
   isEqual,
-  isArray
+  isArray,
+  lowerCaseOf
 } from "@7urtle/lambda";
 import createLogger from "@7urtle/logger";
 import commander from 'commander';
@@ -59,33 +60,55 @@ const getDocumentationLineContents = line =>
 
 const processLineCodeState = line =>
   isEqual('/**')(line)
-    ? documentation.state = states.DESCRIPTION
+    ? newCodeDocumentation()
     : false;
 
 const positionToWhiteSpaceOrStringLenght = contents => (result => isEqual(result)(undefined) ? lengthOf(contents) : result)(search(' ')(contents));
 
-const getTag = contents => substr(positionToWhiteSpaceOrStringLenght(contents))(1)(contents);
+const getTag = contents => substr(positionToWhiteSpaceOrStringLenght(contents) -1)(1)(contents);
 
-const increaseCounter = () => ++documentation.counter &&
+const getTagContents = contents => isEqual(search(' ')(contents))(undefined) ? 'true' : trim(substr(lengthOf(contents))(positionToWhiteSpaceOrStringLenght(contents))(contents));
+
+const newCodeDocumentation = () =>
+  (documentation.state = states.DESCRIPTION) &&
+  ++documentation.counter &&
   documentation.contents.push({
     description: [],
     tags: {},
     example: []
   });
 
+const processTag = tagContents => tag =>
+  isEqual(lowerCaseOf(tag))('example')
+    ? (documentation.state = states.EXAMPLE)
+    : (documentation.state = states.TAG) && (documentation.contents[documentation.counter].tags[tag] = tagContents)
+
 //const createArrayIfNotArrayAndPush = contents => target => isArray(target) ? target.push(contents) : (target = []) && target.push(contents);
 
 const processDescriptionOrTag = contents =>
   startsWith('@')(contents)
-    ? (lengthOf(contents) > 2 ? documentation.contents[documentation.counter].tags[getTag(contents)] = contents : false)
+    //? (lengthOf(contents) > 2 ? documentation.contents[documentation.counter].tags[getTag(contents)] = getTagContents(contents) : false)
+    ? (lengthOf(contents) > 2 ? processTag(getTagContents(contents))(getTag(contents)) : false)
     : lengthOf(contents) > 1 ? documentation.contents[documentation.counter].description.push(contents) : false;
     //: lengthOf(contents) > 1 ? createArrayIfNotArrayAndPush(contents)(documentation.contents[documentation.counter].description) : false;
 
 const processLineDescriptionState = line =>
   startsWith('*')(line)
     ? isEqual('*/')(line)
-        ? increaseCounter() && (documentation.state = states.CODE)
+        ? documentation.state = states.CODE
         : processDescriptionOrTag(getDocumentationLineContents(line))
+    : documentation.state = states.ERROR;
+
+const processExampleOrTag = contents =>
+    startsWith('@')(contents)
+      ? (lengthOf(contents) > 2 ? processTag(getTagContents(contents))(getTag(contents)) : false)
+      : lengthOf(contents) > 1 ? documentation.contents[documentation.counter].example.push(contents) : false;
+
+const processLineExampleState = line =>
+  startsWith('*')(line)
+    ? isEqual('*/')(line)
+        ? documentation.state = states.CODE
+        : processExampleOrTag(getDocumentationLineContents(line))
     : documentation.state = states.ERROR;
 
 const processLineBasedOnState = state =>
@@ -93,7 +116,9 @@ const processLineBasedOnState = state =>
   .of([
     [states.ERROR, identity],
     [states.CODE, processLineCodeState],
-    [states.DESCRIPTION, processLineDescriptionState]
+    [states.DESCRIPTION, processLineDescriptionState],
+    [states.TAG, processLineDescriptionState],
+    [states.EXAMPLE, processLineExampleState]
   ])
   .match(state);
 
