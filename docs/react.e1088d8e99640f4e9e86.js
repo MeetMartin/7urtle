4fe399719d9b711fd73902ba4eac330cfa0f1b5f
Martin Novak <ragnarecek@gmail.com>
2020-08-28 10:30:58 +1200

    new documentation

--- docs/react.e1088d8e99640f4e9e86.js
+++ docs/react.e1088d8e99640f4e9e86.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(e,t,n){"use strict";e.exports=n(287)},12:function(e,t,n){"use strict";!function e(){if("undefined"!=typeof __REACT_DEVTOOLS_GLOBAL_HOOK__&&"function"==typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.checkDCE){0;try{__REACT_DEVTOOLS_GLOBAL_HOOK__.checkDCE(e)}catch(e){console.error(e)}}}(),e.exports=n(288)},287:function(e,t,n){"use strict";
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{1:function(e,t,n){"use strict";e.exports=n(287)},11:function(e,t,n){"use strict";!function e(){if("undefined"!=typeof __REACT_DEVTOOLS_GLOBAL_HOOK__&&"function"==typeof __REACT_DEVTOOLS_GLOBAL_HOOK__.checkDCE){0;try{__REACT_DEVTOOLS_GLOBAL_HOOK__.checkDCE(e)}catch(e){console.error(e)}}}(),e.exports=n(288)},287:function(e,t,n){"use strict";
 /** @license React v16.13.1
  * react.production.min.js
  *
