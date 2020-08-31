fc76935ab7bc000c1ceac27b3b15005fdae7e370
Martin Novak <ragnarecek@gmail.com>
2020-09-01 10:25:51 +1200

    finish improving code documentation for Core functions

--- lambda/src/core.js
+++ lambda/src/core.js
@@ -80,7 +80,7 @@ export const pipe = (...fns) => anything => reduce(anything)((v, f) => f(v))(fns
  * You should use functorMap when you want to work with functors using functions
  * and functional composition rather than calling maps.
  *
- * The function can be called both as unary functorMap(fn)(functor) and binary functorMap(fn, functor).
+ * The function can be called both as a unary functorMap(fn)(functor) and binary functorMap(fn, functor).
  *
  * @HindleyMilner functorMap :: (a -> b) -> Functor -> Functor
  *
@@ -91,11 +91,14 @@ export const pipe = (...fns) => anything => reduce(anything)((v, f) => f(v))(fns
  * @example
  * import {functorMap, Maybe, upperCaseOf} from '@7urtle/lambda';
  *
+ * // the function upperCaseOf is applied to the value of the functor
  * functorMap(upperCaseOf)(Maybe.of('something')); // => Just('SOMETHING')
  *
- * Maybe.of('something').map(upperCaseOf).value === functorMap(upperCaseOf)(Maybe.of('something'));
+ * // use of functorMap equals the use of map on the functor
+ * functorMap(upperCaseOf)(Maybe.of('something')).value === Maybe.of('something').map(upperCaseOf).value;
  *
- * functorMap(upperCaseOf)(Maybe.of('something')) === functorMap(upperCaseOf, Maybe.of('something'));
+ * // functorMap can be called both as a curried unary function or as a standard binary function
+ * functorMap(upperCaseOf)(Maybe.of('something')).value === functorMap(upperCaseOf, Maybe.of('something')).value;
  */
 export const functorMap = nary(fn => functor => functor.map(fn));
 
@@ -105,7 +108,7 @@ export const functorMap = nary(fn => functor => functor.map(fn));
  * You should use functorFlatMap when you want to work with functors using functions
  * and functional composition rather than calling flatMaps.
  *
- * The function can be called both as unary functorFlatMap(fn)(functor) and binary functorFlatMap(fn, functor).
+ * The function can be called both as a unary functorFlatMap(fn)(functor) and binary functorFlatMap(fn, functor).
  *
  * @HindleyMilner functorFlatMap :: (a -> Functor) -> Functor -> Functor
  *
@@ -114,37 +117,107 @@ export const functorMap = nary(fn => functor => functor.map(fn));
  * @return {functor}
  *
  * @example
- * import {functorFlatMap, functorMap, Maybe, upperCaseOf} from '@7urtle/lambda';
+ * import {functorFlatMap, functorMap, Maybe} from '@7urtle/lambda';
  *
  * const maybePlus2 = number => Maybe.of(number + 2);
  *
+ * // the function maybePlus2 is applied to the value of the functor
  * functorFlatMap(maybePlus2)(Maybe.of(3)); // => Just(5)
  * functorMap(maybePlus2)(Maybe.of(3)); // => Just(Just(5))
  *
- * Maybe.of(3).flatMap(maybePlus2).value === functorFlatMap(maybePlus2)(Maybe.of(3));
+ * // use of functorFlatMap equals the use of flatMap on the functor
+ * functorFlatMap(maybePlus2)(Maybe.of(3)).value === Maybe.of(3).flatMap(maybePlus2).value;
  *
- * functorFlatMap(maybePlus2)(Maybe.of(3)) === functorFlatMap(maybePlus2, Maybe.of(3));
+ * // functorFlatMap can be called both as a curried unary function or as a standard binary function
+ * functorFlatMap(maybePlus2)(Maybe.of(3)).value === functorFlatMap(maybePlus2, Maybe.of(3)).value;
  */
 export const functorFlatMap = nary(fn => functor => functor.flatMap(fn));
 
 /**
- * liftA2 (a -> b -> c) -> Applicative a -> Applicative b -> Applicative c
+ * liftA2 provides point-free way of writing calls over applicative functors and functions expecting 2 inputs. It
+ * applies input function over both functors values providing a resulting functor.
  *
- * liftA2 provides point-free way of writing calls over applicative functors and functions with arity 2.
+ * The function can be called both as a unary liftA2(fn)(functor)(functor) and ternary liftA2(fn, functor, functor).
+ *
+ * @HindleyMilner liftA2 (a -> b -> c) -> Applicative a -> Applicative b -> Applicative c
+ *
+ * @pure
+ * @param {function} fn
+ * @param {functor} ap1
+ * @param {functor} ap2
+ * @return {functor}
+ *
+ * @example
+ * import {liftA2, Maybe} from '@7urtle/lambda';
+ *
+ * const add = a => b => a + b;
+ *
+ * // function add which expects two inputs is applied to the values of two applicative functors Maybe
+ * // the result is a Maybe functor with the internal value 5
+ * liftA2(add)(Maybe.of(2))(Maybe.of(3)); // => Just(5)
+ *
+ * // an example of applying a function over a Maybe of undefined value to demonstrate continued safety of functors
+ * liftA2(add)(Maybe.of(1))(Maybe.of(undefined)).isNothing(); // => true
+ *
+ * // liftA2 can be called both as a curried unary function or as a standard ternary function
+ * liftA2(add)(Maybe.of(2))(Maybe.of(3)).value === liftA2(add, Maybe.of(2), Maybe.of(3)).value;
  */
 export const liftA2 = nary(fn => ap1 => ap2 => ap1.map(fn).ap(ap2));
 
 /**
- * liftA3 (a -> b -> c -> d) -> Applicative a -> Applicative b -> Applicative c -> Applicative d
+ * liftA3 provides point-free way of writing calls over applicative functors and functions expecting 3 inputs. It
+ * applies input function over input functors values providing a resulting functor.
  *
- * liftA3 provides point-free way of writing calls over applicative functors and functions with arity 3.
+ * The function can be called both as a unary liftA3(fn)(functor)(functor)(functor) and quaternary liftA2(fn, functor, functor, functor).
+ *
+ * @HindleyMilner liftA3 (a -> b -> c -> d) -> Applicative a -> Applicative b -> Applicative c -> Applicative d
+ *
+ * @pure
+ * @param {function} fn
+ * @param {functor} ap1
+ * @param {functor} ap2
+ * @param {functor} ap3
+ * @return {functor}
+ *
+ * @example
+ * import {liftA3, Maybe} from '@7urtle/lambda';
+ *
+ * const add = a => b => c => a + b + c;
+ *
+ * // function add which expects three inputs is applied to the values of three applicative functors Maybe
+ * // the result is a Maybe functor with the internal value 9
+ * liftA3(add)(Maybe.of(2))(Maybe.of(3))(Maybe.of(4)); // => Just(5)
+ *
+ * // an example of applying a function over a Maybe of undefined value to demonstrate continued safety of functors
+ * liftA3(add)(Maybe.of(1))(Maybe.of(2))(Maybe.of(undefined)).isNothing(); // => true
+ *
+ * // liftA3 can be called both as a curried unary function or as a standard quaternary function
+ * liftA3(add)(Maybe.of(2))(Maybe.of(3))(Maybe.of(4)).value === liftA3(add, Maybe.of(2), Maybe.of(3), Maybe.of(4)).value;
  */
 export const liftA3 = nary(fn => ap1 => ap2 => ap3 => ap1.map(fn).ap(ap2).ap(ap3));
 
 /**
- * concat :: a -> a|boolean
- * 
  * contact outputs concatenated inputs of strings, arrays and objects or outputs undefined for other types.
+ *
+ * concat can be called both as a curried unary function or as a standard binary function.
+ *
+ * @HindleyMilner concat :: a -> a|boolean
+ *
+ * @pure
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @example
+ * import {concat} from '@7urtle/lambda';
+ *
+ * concat('cd')('ab'); // => 'abcd'
+ * concat([3, 4])([1,2]); // => [1, 2, 3, 4]
+ * concat({here: {here: 'there'}})({hi: 'hello'}); // => {hi: 'hello', here: {here: 'there'}}
+ * concat('cd')(1); // => undefined
+ *
+ * // concat can be called both as a curried unary function or as a standard binary function
+ * concat('cd')('ab') === concat('cd', 'ab');
  */
 export const concat = nary(a => b =>
   isString(b) || isArray(b)
@@ -154,30 +227,102 @@ export const concat = nary(a => b =>
       : undefined);
 
 /**
- * includes :: a -> b -> boolean
+ * includes(a)(b) output is true if b includes a.
  *
- * includes output is true if b includes a.
+ * includes can be called both as a curried unary function or as a standard binary function.
+ *
+ * @HindleyMilner includes :: a -> b -> boolean
+ *
+ * @pure
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @example
+ * import {includes} from '@7urtle/lambda';
+ *
+ * includes('rt')('7urtle'); // => true
+ * includes(1)([1, 2, 3])) // => true
+ * includes('turtle')([1, 2, 3])) // => false
+ *
+ * // includes can be called both as a curried unary function or as a standard binary function
+ * includes('rt')('7urtle') === includes('rt', '7urtle');
  */
 export const includes = nary(a => b => b.includes(a));
 
 /**
- * indexOf :: a -> b -> number
- * 
- * indexOf outputs position of input a within input b or undefined if it is not found.
+ * indexOf(a)(b) outputs position of input a within input b or undefined if it is not found.
+ *
+ * indexOf can be called both as a curried unary function or as a standard binary function.
+ *
+ * @HindleyMilner indexOf :: a -> b -> number
+ *
+ * @pure
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @example
+ * import {indexOf} from '@7urtle/lambda';
+ *
+ * indexOf('7')('7urtle'); // => 0
+ * indexOf(7)('7urtle'); // => 0
+ * indexOf(2)([1, 2, 3]); // => 1
+ * indexOf(4)([1, 2, 3]); // => undefined
+ *
+ * // indexOf can be called both as a curried unary function or as a standard binary function
+ * indexOf('7')('7urtle') === indexOf('7', '7urtle');
  */
 export const indexOf = nary(a => b => minusOneToUndefined(b.indexOf(a)));
 
 /**
- * lastIndexOf :: a -> b -> number
- * 
- * lastIndexOf outputs position of input a withing input b looking from the end or it retuns undefined if it is not found.
+ * lastIndexOf(a)(b) outputs position of input a withing input b looking from the end or it retuns undefined if it is not found.
+ *
+ * lastIndexOf can be called both as a curried unary function or as a standard binary function.
+ *
+ * @HindleyMilner lastIndexOf :: a -> b -> number
+ *
+ * @pure
+ * @param {*} a
+ * @param {*} b
+ * @return {*}
+ *
+ * @example
+ * import {lastIndexOf} from '@7urtle/lambda';
+ *
+ * lastIndexOf('urtle')('7urtle'); // => 1
+ * lastIndexOf(2)([1, 2, 3, 2]); // => 3
+ * lastIndexOf('8')('7urtle'); // => undefined
+ *
+ * // lastIndexOf can be called both as a curried unary function or as a standard binary function
+ * lastIndexOf('7')('7urtle') === lastIndexOf('7', '7urtle');
  */
 export const lastIndexOf = nary(a => b => minusOneToUndefined(b.lastIndexOf(a)));
 
 /**
- * memoize :: object -> (a -> b) -> a -> b
+ * memoize uses input memory to save output of input function and then uses it to lookup the result on a repeated run.
  *
- * memoize uses input memory to save output of input function and then uses it to lookup result on a repeated run
+ * The function can be called both as a curried unary function or as a standard ternary function
+ *
+ * @HindleyMilner memoize :: object -> (a -> b) -> a -> b
+ *
+ * @param {object} memory
+ * @param {function} fn
+ * @param {*} anything
+ * @return {*}
+ *
+ * @example
+ * import {memoize} from '@7urtle/lambda';
+ *
+ * const addTwo = a => a + 2;
+ * let memory = {};
+ *
+ * memoize(memory)(addTwo)(1); // => 3
+ * memoize(memory)(addTwo)(1); // => 3
+ * memory[1]; // => 3
+ *
+ * // lastIndexOf can be called both as a curried unary function or as a standard ternary function
+ * memoize(memory)(addTwo)(1) === memoize(memory, addTwo, 1);
  */
-export const memoize = nary(memory => fn => a =>
-  a in memory ? memory[a] : (passThrough(b => memory[a] = b))(fn(a)));
+export const memoize = nary(memory => fn => anything =>
+    anything in memory ? memory[anything] : (passThrough(b => memory[anything] = b))(fn(anything)));
