20af1850f7da9606d31f258cd1d74e2f2c8512e2
Martin Novak <ragnarecek@gmail.com>
2020-09-07 10:44:08 +1200

    Maybe documented

--- lambda/src/Maybe.js
+++ lambda/src/Maybe.js
@@ -3,20 +3,58 @@ import {isNothing} from "./conditional";
 import {nary} from "./arity";
 
 /**
- * Maybe.of() outputs instance of Maybe.
- * Maybe.of(a).inspect() outputs string Just(a) or Nothing.
- * Maybe.of(a) outputs Nothing for an input that is null, undefined, an empty string or an empty array.
- * Maybe.of(a) outputs Just for an input a that is not Nothing.
- * Maybe.of(a) outputs Nothing for an input a that is null or undefined.
- * Maybe.of(a).isJust() of an input a outputs true for a value that is Just.
- * Maybe.of(a).isNothing() of an input a outputs true for a value that is Nothing.
- * Maybe.of(a).map(a -> b) executes function over Maybe input a.
- * Maybe.of(a).map(a -> Just) outputs Just(Just).
- * Maybe.of(a).map(a -> b) does not execute over Nothing.
- * Maybe.of(a).flatMap(a -> b) executes function over Maybe input a returns its raw value.
- * Maybe.of(a).flatMap(a -> b) does not execute over Nothing.
- * Maybe.of(a).map(a -> b).ap(Maybe) provides applicative ability to apply functors to each other.
- * Maybe.of(Maybe -> Maybe -> c).ap(Maybe).ap(Maybe) provides applicative interface for a functor of a function.
+ * Maybe is one of the simplest and well known monads. In other languages or libraries it is also sometimes
+ * called Option.
+ *
+ * Maybe expects a value as it's input. It is Nothing if the value is null, undefined, or empty. It returns
+ * Just for all other cases.
+ *
+ * Maybe is called Maybe because it maybe holds a value. You want to use Maybe for situations when you don't
+ * know whether there is going to be an output. It makes the situation very obvious and forces its consumers
+ * to safely deal with it.
+ *
+ * @example
+ * import {maybe, Maybe, upperCaseOf, liftA2} from '@7urtle/lambda';
+ *
+ * // in the example we randomly give Maybe a value or undefined. Maybe.of() outputs an instance of Maybe.
+ * const myMaybe = Maybe.of(Math.random() > 0.5 ? 'random success' : undefined);
+ *
+ * // you could access the actual value like this
+ * myMaybe.value; // => 'random success' or undefined
+ *
+ * // you can also inspect it by
+ * myMaybe.inspect(); // => "Just('random success')" or "Nothing"
+ *
+ * // you can check if the value is Nothing
+ * myMaybe.isNothing(); // => true or false
+ * Maybe.of('abc').isNothing(); // => false
+ * Maybe.of([]).isNothing(); // => true
+ *
+ * // you can check if the value is Just
+ * myMaybe.isJust(); // => true or false
+ * Maybe.of(123).isJust(); // => true
+ * Maybe.of(null).isJust(); // => false
+ *
+ * // as a functor the value inside is safely mappable (map doesn't execute over Nothing)
+ * myMaybe.map(value => upperCaseOf(value));
+ * myMaybe.inspect(); // => "Just('RANDOM SUCCESS')" or "Nothing"
+ *
+ * // as a monad Maybe can be safely flat mapped with other Maybes (flatMap doesn't execute over Nothing)
+ * Maybe.of(3).flatMap(a => Maybe.of(a + 2)).inspect(); // => 'Just(5)'
+ * Maybe.of(3).flatMap(a => Maybe.of(null)).inspect(); // => 'Nothing'
+ * Maybe.of(3).flatMap(a => a + 2); // => 5
+ *
+ * // as applicative functor you can apply functors to each other especially using liftA2 or liftA3
+ * const add = a => b => a + b;
+ * liftA2(add)(Maybe.of(2))(Maybe.of(3)); // => Just(5)
+ * Maybe.of(1).map(add).ap(Maybe.of(2)).inspect(); // => 'Just(3)'
+ * Maybe.of(1).map(add).ap(Maybe.of(null)).inspect(); // => 'Nothing'
+ * Maybe.of(add).ap(Maybe.of(1)).ap(Maybe.of(2)).inspect(); // => 'Just(3)'
+ *
+ * // as an example you can use Maybe to help you work with DOM like this
+ * Maybe.of(document.querySelector('#iexist')).map(a => a.offsetTop); // => Just(1240)
+ * Maybe.of(document.querySelector('#idontexist')).map(a => a.offsetTop); // => Nothing
+ * maybe('error: the object doesnt exist')(a => 'offset from top is ' + a)(Maybe.of(document.querySelector('#iexist')).map(a => a.offsetTop))
  */
 export class Maybe {
   constructor(x) {
@@ -81,9 +119,26 @@ class Nothing extends Maybe {
 }
 
 /**
- * maybe :: a -> (b -> c) -> Maybe
- *
  * maybe outputs result of a function onJust if input Maybe is Just or outputs input error if input Maybe is Nothing.
+ *
+ * maybe can be called both as a curried unary function or as a standard binary function.
+ *
+ * @HindleyMilner maybe :: a -> (b -> c) -> Maybe
+ *
+ * @pure
+ * @param {*} a
+ * @param {function} b
+ * @return {Maybe}
+ *
+ * @example
+ * import {maybe, Maybe} from '@7urtle/lambda';
+ *
+ * maybe('error')(a => a)(Maybe.of('abc')); // => 'abc'
+ * maybe('error')(a => a)(Maybe.of(undefined)); // => 'error'
+ * maybe('error')(a => a)(Maybe.of(undefined)) === Maybe.of(undefined).isNothing() ? 'error' ? 'not error';
+ *
+ * // maybe can be called both as a curried unary function or as a standard binary function
+ * maybe('error')(a => a)(Maybe.of('abc')) === maybe('error', a => a, Maybe.of('abc'));
  */
 export const maybe = nary(error => onJust => functorMaybe =>
   functorMaybe.isNothing()
