7b6c775cfced1b5bf4c2ff772bb3d9886c85beb8
Martin Novak <ragnarecek@gmail.com>
2020-09-07 09:10:32 +1200

    nary documentation

--- lambda/src/arity.js
+++ lambda/src/arity.js
@@ -7,6 +7,21 @@
  * @param {function} fn
  * @returns {function}
  */
+/**
+ * nary takes input of a curried function and allows it to be called both as curried and n-ary.
+ *
+ * @HindleyMilner nary :: (a -> b) -> a -> b
+ *
+ * @pure
+ * @param {function} fn
+ * @return {function}
+ *
+ * @example
+ * import {nary} from '@7urtle/lambda';
+ *
+ * const fn1 = nary(a => b => a + b);
+ * fn1('a')('b') === fn1('a', 'b'); // => true
+ */
 export const nary = fn =>
     (...args) => args.length === 0
         ? fn()
