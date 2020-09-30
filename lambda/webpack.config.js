6161f80a837a6e4c438250dd0f20576d010fb07f
Martin Novak <ragnarecek@gmail.com>
2020-10-01 09:13:16 +1300

    Lambda is L

--- lambda/webpack.config.js
+++ lambda/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'lambda.min.js',
-    library: 'lambda',
+    library: 'L',
     libraryTarget: 'umd',
     globalObject: 'this'
   },
